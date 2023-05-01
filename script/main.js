2a88f27b50d6aecd779b83004298f6cb6a83ec96
MuhammadSalimov <salimovmuhammad2322@gmail.com>
2023-05-01 09:57:27 +0500

    finished

--- script/main.js
+++ script/main.js
@@ -1,5 +1,11 @@
 const cube = document.querySelectorAll('.cube')
 const span= document.querySelectorAll('span')
+const done = document.querySelector('.done')
+const formSection = document.querySelector('.form-section')
+const btnDone  = document.querySelector('.btn-done')
+const cubicSection = document.querySelector('.cubic-section')
+const Formtg = document.getElementById('tg')
+
 cube.forEach((e)=>{
     e.addEventListener('click' ,(elem)=> {
         const parent= elem.target.parentElement
@@ -7,8 +13,27 @@ cube.forEach((e)=>{
         setTimeout(function spanShow(){
             span.forEach((e)=>{
                 e.classList.add('show')
+                setTimeout(()=>{
+                    done.style.left="0%"
+                    setTimeout(()=>{
+                        cubicSection.style.display="none"
+                        formSection.style.display="block"
+                    },4000)
+                },1000)
             })
         },1000)
+        
+        spanShow()
     })
 })
-spanShow()
+
+
+
+btnDone.addEventListener('click' , ()=>{
+    cubicSection.style.display="none"
+    formSection.style.display="block"
+})
+
+
+
+
